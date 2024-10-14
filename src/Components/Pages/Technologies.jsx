e047d840aec2944ef9e600797bcc63f02461c106
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-14 20:08:37 +0530

    Technologies tag updated

--- src/Components/Pages/Technologies.jsx
+++ src/Components/Pages/Technologies.jsx
@@ -25,7 +25,7 @@ import {
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h1 className="my-20 text-center text-4xl">Technologies</h1>
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
 
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
