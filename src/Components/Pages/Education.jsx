660a844f0ddda912df5c0137965781fb91964abb
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-15 16:47:40 +0530

    Adding Framer-Motion in Education

--- src/Components/Pages/Education.jsx
+++ src/Components/Pages/Education.jsx
@@ -1,16 +1,34 @@
 import { EDUCATION } from "../../constants";
+import { motion } from "framer-motion";
 
 const Education = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Education</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Education
+      </motion.h2>
       <div>
         {EDUCATION.map((education, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
               <p className="mb-2 text-sm text-neutral-400">{education.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">
                 {education.degree} -{" "}
                 <span className="text-sm text-purple-100">
@@ -18,7 +36,7 @@ const Education = () => {
                 </span>
               </h6>
               <p className="mb-4 text-neutral-400">{education.description}</p>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
