753792eb9e69ddc03cdeb55d231efc2fa39c5d67
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-15 16:44:24 +0530

    Adding Framer-Motion in Training

--- src/Components/Pages/Training.jsx
+++ src/Components/Pages/Training.jsx
@@ -1,16 +1,34 @@
 import { TRAINING } from "../../constants";
+import { motion } from "framer-motion";
 
 const Training = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Experience</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Training
+      </motion.h2>
       <div>
         {TRAINING.map((training, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
               <p className="mb-2 text-sm text-neutral-400">{training.year}</p>
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">
                 {training.role} -{" "}
                 <span className="text-sm text-purple-100">
@@ -26,7 +44,7 @@ const Training = () => {
                   {tech}
                 </span>
               ))}
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
