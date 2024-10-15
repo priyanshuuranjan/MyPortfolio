7ab36d004ee9907a571253531f1f764856fcec31
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-15 17:03:55 +0530

    Adding Framer-Motion in Project

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -1,17 +1,30 @@
 import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
 import { PROJECTS } from "../../constants";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div
             key={index}
             className="mb-8 flex flex-col lg:flex-row lg:justify-center items-center"
           >
-            <div className="w-full lg:w-1/3 flex justify-center lg:justify-start">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/3 flex justify-center lg:justify-start"
+            >
               <img
                 src={project.image}
                 width={250}
@@ -19,11 +32,15 @@ const Projects = () => {
                 alt={project.title}
                 className="mb-6 rounded"
               />
-            </div>
+            </motion.div>
 
-            <div className="w-full max-w-xl lg:w-2/3 lg:ml-8">
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-2/3 lg:ml-8"
+            >
               <h6 className="mb-2 font-semibold text-lg text-white flex items-center">
-                {project.title}
                 <a
                   href={project.githubLink}
                   target="_blank"
@@ -52,7 +69,7 @@ const Projects = () => {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
