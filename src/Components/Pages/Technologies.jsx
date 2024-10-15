2c0b7bede49a6d5009857a42373634bdd91acf87
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-15 16:37:45 +0530

    Adding Framer-Motion in Tech

--- src/Components/Pages/Technologies.jsx
+++ src/Components/Pages/Technologies.jsx
@@ -24,86 +24,210 @@ import {
   SiPostman,
   SiRender,
 } from "react-icons/si";
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10], // Bounces bet 10 and -10  in the y-axis
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse", // reverses the direction after the each animation
+    },
+  },
+});
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Technologies
+      </motion.h2>
 
-      <div className="flex flex-wrap items-center justify-center gap-4">
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+      <motion.div
+        whileInView={{ opacity: 1, x: 0 }}
+        initial={{ opacity: 0, x: -100 }}
+        transition={{ duration: 1.5 }}
+        className="flex flex-wrap items-center justify-center gap-4"
+      >
+        <motion.div
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <img src={cppImage} alt="C++ logo" className="h-16 w-16" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaHtml5 className="text-7xl text-yellow-700" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaBootstrap className="text-7xl text-blue-800" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiTailwindCssFill className="text-7xl text-cyan-500" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiJavascript className="text-7xl text-yellow-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiReactjsLine className="text-7xl text-cyan-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaDatabase className="text-7xl text-yellow-700" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(7)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMysql className="text-7xl text-blue-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaAws className="text-7xl text-white-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiMongodb className="text-7xl text-green-500" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <RiFirebaseFill className="text-7xl text-yellow-500" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaNode className="text-7xl text-green-500" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(8)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BsFiletypeXml className="text-7xl text-cyan-200" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BsFiletypeJson className="text-7xl text-slate-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FaGithub className="text-7xl text-white-100" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <BiLogoNetlify className="text-7xl text-green-600" />
-        </div>
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        </motion.div>
+
+        <motion.div
+          variants={iconVariants(6)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiRender className="text-7xl text-white-200" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <SiPostman className="text-7xl text-red-400" />
-        </div>
+        </motion.div>
 
-        <div className="rounded-2xl border-4 border-neutral-800 p-4">
+        <motion.div
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
           <FiFigma className="text-7xl text-red-700" />
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </div>
   );
 };
