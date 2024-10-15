d690069b6ba7ef7894bf78b5f8135c0a3452fe09
Priyanshu Rajan <priyanshumth0808@gmail.com>
2024-10-15 20:38:10 +0530

    updating Navbar

--- src/Components/Pages/Navbar.jsx
+++ src/Components/Pages/Navbar.jsx
@@ -1,42 +1,37 @@
+import { SiLeetcode } from "react-icons/si";
 import "./Navbar.css";
-import menu_open from "../../assets/menu_open.svg";
-import menu_close from "../../assets/menu_close.svg";
-import { useRef } from "react";
+
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
-  const menuRef = useRef();
-
-  const openMenu = () => {
-    menuRef.current.style.right = "0";
-  };
-  const closeMenu = () => {
-    menuRef.current.style.right = "-350px";
-  };
-
   return (
     <nav className="mb-20 flex item-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
-        <div className="mx-2 w-10 text-xl">Priyanshu</div>
+        <div className="mx-2 w-10 text-2xl">Priyanshu</div>
       </div>
-      <img src={menu_open} onClick={openMenu} alt="" className="nav-mob-open mt-7"/>
 
-      <ul ref={menuRef} className="nav-menu">
-        <img
-          src={menu_close}
-          onClick={closeMenu}
-          alt=""
-          className="nav-mob-close mt-7"
-        />
-        <li>Home</li>
-        <li>About Me</li>
-        <li>Services</li>
-        <li>Project</li>
-        <li>Contact</li>
-      </ul>
-      <div className="m-8 flex item-center justify-center gap-5 text-2xl ">
-        <FaLinkedin />
-        <FaGithub />
+      <div className="m-5 flex item-center justify-center gap-5 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/priyanshuranjan45/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin />
+        </a>
+        <a
+          href="https://github.com/priyanshuuranjan"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub />
+        </a>
+        <a
+          href="https://leetcode.com/u/priyanshu_45/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <SiLeetcode />
+        </a>
       </div>
     </nav>
   );
